9ae4493f18d32fa229310a861b197aba9a193a6a
Jackie Amador <jamador01@saintmarys.edu>
2017-06-14 06:02:39 -0400

    ArticleTitle Comp and Author Comp inside ArticleBody Comp: 6 different components made

--- src/ArticleBody.js
+++ src/ArticleBody.js
@@ -1,21 +1,18 @@
 import React, { Component } from 'react';
 import './ArticleBody.css'
+import Author from './Author'
 import ArticleText from './ArticleText'
+import ArticleTitle from './ArticleTitle'
+
 
 class ArticleBody extends Component {
     render() {
         return (
         <main className="expanded row">
           <div className="large-8 medium-12 columns article">
-            <h2 className="article-title">Gold Madness - Fact or Fiction?</h2>
-            <div className="avatar">
-              <img src="http://www.zbrushcentral.com/attachment.php?attachmentid=376082" alt="author" />
-              <div className="author-info">
-                <p className="author-name">By Domri, son of Flug</p>
-                <p className="date">on 28 April, 3018 of the Third Age</p>
-              </div>
-            </div>
-<ArticleText />
+            <ArticleTitle />
+            <Author />
+            <ArticleText />
             <div className="article-links">
               <a className="article-link" href="#">
                 <i className="fa fa-comments-o"></i>
