50b0e8741f9679b246d7d8af764ed632d6dfa11d
Jackie Amador <jamador01@saintmarys.edu>
2017-06-14 04:18:49 -0400

    Footer Component added

--- src/App.js
+++ src/App.js
@@ -2,18 +2,17 @@ import React, { Component } from 'react';
 import './App.css';
 import Header from './Header'
 import ArticleBody from './ArticleBody'
+import Footer from './Footer'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Header />
+
         <ArticleBody />
 
-        <footer className="expanded row">
-          <h6>&copy; 2017 Erebor Industries</h6>
-          <h6 className="float-right">All rights reserved</h6>
-        </footer>
+        <Footer />
 
       </div>
     );
